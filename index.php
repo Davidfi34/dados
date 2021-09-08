1e2b9bb131470304bb961245c7438fc4880bfdb9
Facundo <facugastonbarral@gmail.com>
2021-09-08 20:36:33 -0300

    Consigna (index.html)

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lanzar dados</title>
+</head>
+<body>
+    <!-- Generar dos numeros aleatorios simulando ser dados en PHP y HTML -->
+</body>
+</html>
